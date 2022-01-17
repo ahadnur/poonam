cd5cd9605986364566c155e4c3cfca14d3f34564
ahadnur <ahadnur44@gmail.com>
2022-01-18 00:45:00 +0600

    code for poonam

---
+++ main.js
@@ -0,0 +1,66 @@
+// this is a comment
+// window.alert
+// innerHTML
+// document.write
+// console.log();
+
+
+// Arithmatic Operators
+// +, -, *, /, %, ++, --, **
+
+// Assignment Operators
+// =, +=, -=, *=, /=, **=
+
+
+// let number1 = 32 % 10    //number
+// let number2 = 20
+
+// number2 = number2 + 10
+// console.log(number2)
+// console.log(number2 += 10)
+
+// let result = (number1 + number2) * 10
+
+// let name = 19 + 18     // string
+
+// let person = {
+//     name: 'John',
+//     age: 34
+// }
+
+
+// console.log(2**3)
+
+
+
+
+
+
+// {} --> condition, function, object
+
+
+// console.log(2 == 3)
+
+// Comparison Operators
+
+// let number1 = 1
+// let number2 = 3
+
+// console.log(number1 < number2)
+//  ==  --> it will check two entites no type
+// ===  --> It will check the type
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+
+
+// Logical Operators
+
+// logiacl and      --> &&
+// Logical or       --> ||
+// Logical not      --> !
+
+// console.log();
